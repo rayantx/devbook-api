4f6e4b994be16d094e623e8a6d0e22c26e97a2ec
Rayan T <rayanzax@hotmail.com>
2024-06-24 23:10:09 -0300

    Commit inicial

---
+++ api/go.mod
@@ -0,0 +1,19 @@
+module api
+
+go 1.22.3
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/badoux/checkmail v1.2.4
+
+require golang.org/x/crypto v0.24.0
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1
+)
---
+++ api/src/router/rotas/rotas.go
@@ -0,0 +1,25 @@
+package rotas
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Rota struct {
+	URI                string
+	Metodo             string
+	Funcao             func(http.ResponseWriter, *http.Request)
+	RequerAutenticacao bool
+}
+
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+	rotas = append(rotas, rotaLogin)
+
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	}
+
+	return r
+}
